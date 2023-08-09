6a42bdaccf96e1317c114fd0e60c0d340924d779
Emerson Marins <emersonmarinscoutinho@gmail.com>
2023-08-09 00:19:02 -0300

    added delete one and delete many methods

---
+++ deleteMany_method/delete_many.py
@@ -0,0 +1,39 @@
+import os
+import pprint
+ 
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Import ObjectId from bson package (part of PyMongo distribution) to enable querying by ObjectId
+from bson.objectid import ObjectId
+
+# Load config from .env file 
+load_dotenv()
+MONGODB_URI = os.environ['URI']
+
+# Connect to MongoDB cluster with MongoClient
+client =  MongoClient(MONGODB_URI)
+
+# Get a reference to 'bank' database
+db = client.bank
+
+# Get a reference to the 'accounts' collection
+accounts_collection = db.accounts
+
+# Filter for accounts with balance less than $2000
+documents_to_delete = {"balance": {"$lt": 200}}
+
+# Search for sample document before delete
+print("Searching for sample target document before delete: ")
+pprint.pprint(accounts_collection.find_one(documents_to_delete))
+
+# Write an expression that deletes the target accounts.
+result = accounts_collection.delete_many(documents_to_delete)
+
+# Search for sample document after delete
+print("Searching for sample target document after delete: ")
+pprint.pprint(accounts_collection.find_one(documents_to_delete))
+
+print("Documents deleted: " + str(result.deleted_count))
+ 
+client.close()
---
+++ deleteOne_method/delete_one.py
@@ -0,0 +1,39 @@
+import os
+import pprint
+ 
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Import ObjectId from bson package (part of PyMongo distribution) to enable querying by ObjectId
+from bson.objectid import ObjectId
+
+# Load config from .env file 
+load_dotenv()
+MONGODB_URI = os.environ['URI']
+
+# Connect to MongoDB cluster with MongoClient
+client =  MongoClient(MONGODB_URI)
+
+# Get a reference to 'bank' database
+db = client.bank
+
+# Get a reference to the 'accounts' collection
+accounts_collection = db.accounts
+
+# Filter by ObjectId
+document_to_delete = { "_id": ObjectId("64d1bcc9326925ce273fab20") }
+
+# Search for document before delete
+print(" Searching for target document before delete: ")
+pprint.pprint(accounts_collection.find_one(document_to_delete))
+
+# Write an expression that deletes the target account.
+result = accounts_collection.delete_one(document_to_delete)
+
+# Search for document after delete
+print(" Searching for target document after delete: ")
+pprint.pprint(accounts_collection.find_one(document_to_delete))
+
+print("Documents deleted: " + str(result.deleted_count))
+  
+client.close()
